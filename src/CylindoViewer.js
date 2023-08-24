c4aaa181d591bb623dd2c152187d878af34eaae8
Edsonxn <jairedson5@gmail.com>
2023-08-24 17:58:07 -0600

    Mensaje descriptivo del commit

---
+++ src/CylindoViewer.js
@@ -0,0 +1,24 @@
+import React, { useRef, useLayoutEffect } from 'react';
+import './styles.css'; // Importar los estilos personalizados
+
+
+
+function CylindoViewer({ styles }) {
+  const viewer = useRef(null);
+
+  useLayoutEffect(() => {
+    if (viewer.current) {
+      viewer.current.features = styles;
+    }
+  }, [styles]);
+
+  return (
+    <cylindo-viewer
+      ref={viewer}
+      customer-id="5098"
+      code="CONFIGURABLE-SECTIONAL-PDP"
+    />
+  );
+}
+
+export default CylindoViewer;
